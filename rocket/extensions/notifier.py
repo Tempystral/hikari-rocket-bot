0e3bb9ff9ad333357e4ce1336258d8e96d4955af
Tempystral <elijah.doern@gmail.com>
2023-01-16 23:49:10 -0500

    implement bot commands

--- rocket/extensions/notifier.py
+++ rocket/extensions/notifier.py
@@ -1,26 +1,69 @@
+from logging import getLogger
+import hikari
 import lightbulb as lb
+
+from twitchAPI.helper import first
+from twitchAPI.oauth import get_user_info
+
 from rocket.extensions.checks import has_elevated_role_in_guild
-twitch_plugin = lb.Plugin("notifier")
+from rocket.twitch import TwitchHelper
+from rocket.util.config import ServerConfig
+from rocket.util.errors import RocketBotException
+
+log = getLogger("rocket.extensions.twitch")
+
+twitch_plugin = lb.Plugin("twitch")
 
 @twitch_plugin.command
 @lb.add_checks(has_elevated_role_in_guild())
-@lb.command("twitch", "Perform Twitch-related actions", hidden=False)
+@lb.command("twitch", "Perform Twitch-related actions", hidden=False, guilds=[645680257797586953, 697911541193769021])
 @lb.implements(lb.SlashCommandGroup, lb.PrefixCommandGroup)
 async def twitch_group(ctx: lb.Context) -> None:
   pass
 
 @twitch_group.child
+@lb.option("username", "Your Twitch username", type=str)
 @lb.command("authorize", "Perform Twitch-related actions", aliases=["auth"], hidden=False)
-@lb.implements(lb.SlashCommandGroup, lb.PrefixCommandGroup)
-async def authorize_user(ctx: lb.Context):
-  # Look at oauth.py > __handle_callback() and authenticate() from pyTwitchAPI for inspiration
-  pass
-
-@twitch_group.child
-@lb.option("channel", "Twitch channel to receive notifications about", type=str)
-@lb.command("add", "Add a Twitch channel to the bot's notification list", inherit_checks=True)
 @lb.implements(lb.SlashSubCommand, lb.PrefixSubCommand)
-async def add_stream_online_notifications(ctx: lb.Context):
+async def authorize_user(ctx: lb.Context):
+  helper: TwitchHelper = ctx.bot.d.helper
+  settings: ServerConfig = ctx.bot.d.settings
+
+  log.info(f"{ (settings is ctx.bot.d.settings) = }")
+
+  tokens = None
+
+  assert ctx.options.username
+  assert ctx.guild_id
+
+  if ctx.options.username in settings.get_all_users():
+    if ctx.options.username not in settings.get_guild(ctx.guild_id).watching:
+      settings.add_user(ctx.guild_id, ctx.options.username)
+    # Attempt to re-validate
+    if (user := settings.get_user(ctx.options.username)):
+      tokens = await helper.validate(user)
+  
+  if not tokens: # User is not in the list or has invalid tokens
+    await ctx.respond(f"You need to authorize Rocketbot with Twitch. Please sign in at <{helper.userauth.return_auth_url()}>", flags=hikari.MessageFlag.EPHEMERAL)
+    tokens = await helper.authenticate(ctx.options.username)
+  
+  user = await first(helper.twitch.get_users(logins=[ctx.options.username]))
+  
+  if tokens and user:
+    userinfo:dict[str,str] = await get_user_info(tokens[0])
+    log.debug(f"User retrieved: {userinfo.get('preferred_username')} | id: {userinfo.get('sub')}")
+    if userinfo.get("sub") == user.id:
+      log.debug(f"Verified: {user.display_name} is correct user, adding to list of users")
+      settings.set_user_tokens(ctx.options.username, int(user.id), user.display_name, tokens[0], tokens[1])
+
+    #log.warning(user.to_dict(include_none_values=True))
+      
+    log.warning(settings.get_user(ctx.options.username))
+    await ctx.respond(
+      hikari.Embed(description=f"User successfully authenticated!", colour="#6441a5")
+      .set_author(name=user.display_name, icon=user.profile_image_url),
+      flags=hikari.MessageFlag.EPHEMERAL)
+
   '''
   - Check to see whether this user is authenticated in this or another server
     - If they are authenticated in another server, copy the details from that server's list to this one
@@ -31,15 +74,14 @@ async def add_stream_online_notifications(ctx: lb.Context):
   - Add a subscription for the user in EventSub
   '''
 
-  # This function might require some restructuring of the settings.json file
-  # It'll probably be easier to make each server a dict with the guild ID as a key, and to store a dict for each twitch channel as well
-  pass
+@twitch_plugin.set_error_handler
+async def on_error(event: lb.events.CommandErrorEvent) -> bool | None:
+  log.warning(f"Caught exception: {type(event.exception)}")
 
-@twitch_group.child
-@lb.command("test", "Puts the bot in testing mode.", inherit_checks=True)
-@lb.implements(lb.SlashSubCommand, lb.PrefixSubCommand)
-async def remove_stream_online_notifications(ctx: lb.Context):
-  pass
+  # if isinstance(event.exception, lb.errors.CommandInvocationError):
+  if isinstance(event.exception, RocketBotException):
+    await event.context.respond(event.exception.message, flags=hikari.MessageFlag.EPHEMERAL)
+    return True # To tell the bot not to propogate this error event up the chain
 
 def load(bot: lb.BotApp) -> None:
   bot.add_plugin(twitch_plugin)
